48a56806bac8212ec0da243e4a97582bb65d2697
Gor-light <learncz19@gmail.com>
2026-07-09 00:22:38 +0200

    Create shop homepage with products

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -11,6 +11,29 @@ class ShopController extends AbstractController
     #[Route('/', name: 'app_shop_index', methods: ['GET'])]
     public function index(): Response
     {
-        return new Response('Hello shop');
+        return $this->render('shop/index.html.twig', [
+            'products' => $this->getProducts(),
+        ]);
+    }
+
+    private function getProducts(): array
+    {
+        return [
+            [
+                'name' => 'AirPods',
+                'description' => 'Wireless headphones for music and calls.',
+                'price' => 5800,
+            ],
+            [
+                'name' => 'MacBook',
+                'description' => 'Powerful laptop for work, study, and coding.',
+                'price' => 28000,
+            ],
+            [
+                'name' => 'iPhone',
+                'description' => 'Modern smartphone with great camera and performance.',
+                'price' => 33000,
+            ],
+        ];
     }
 }
